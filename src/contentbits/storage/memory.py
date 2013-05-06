330874906f839226e19b13cb6ed2c6b34b78c2e3
Michał Bachowski <michal@bachowski.pl>
2013-05-06 11:20:55 +0200

    Refactored storage/memory module to return instance of Promise class so that asynchronous style of working with storage classes is forced

--- src/contentbits/storage/memory.py
+++ src/contentbits/storage/memory.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from itertools import count
 from contentbits.storage.abstract import Abstract
+from contentbits.utils import as_deferred
 
 
 class Memory(Abstract):
@@ -20,9 +21,11 @@ class Memory(Abstract):
             if idx not in collection:
                 return idx
 
+    @as_deferred
     def read_collection(self, collection_id):
         return self._collections[collection_id]
 
+    @as_deferred
     def create_collection(self, collection_id=None):
         # generate collection_id if not given
         if collection_id is None:
@@ -32,16 +35,19 @@ class Memory(Abstract):
             self._collections[collection_id] = {}
         return collection_id
 
+    @as_deferred
     def store_item(self, collection_id, data, item_id=None):
         if item_id is None:
             item_id = self._get_idx(self._collections[collection_id])
         self._collections[collection_id][item_id] = data
         return item_id
 
+    @as_deferred
     def remove_item(self, collection_id, item_id):
         del self._collections[collection_id][item_id]
-        return self
+        return (collection_id, item_id)
 
+    @as_deferred
     def remove_collection(self, collection_id):
         del self._collections[collection_id]
-        return self
+        return collection_id
--- test/storage/memory_test.py
+++ test/storage/memory_test.py
@@ -8,13 +8,15 @@ import unittest
 from functools import partial
 
 # hack for loading modules
-import _path
-_path.fix(2)
+from mock_helper import IsA
+from _path import fix, mock
+fix(2)
 
 
 ##
 # content bits modules
 #
+from promise import Promise
 from contentbits.storage.memory import Memory
 
 
@@ -22,52 +24,73 @@ class MemoryTestCase(unittest.TestCase):
 
     def setUp(self):
         self.collection = Memory()
+        self.c = mock.MagicMock()
 
-    def test_read_collection_raises_key_error_for_non_existing_collection(self):
-        self.assertRaises(KeyError, partial(self.collection.read_collection,
-                'foo'))
+    def _idx(self, deferred, callback=None):
+        if callback is None:
+            callback = mock.MagicMock()
+        deferred.done(callback)
+        idx = callback.call_args[0][0]
+        return idx
+
+    def test_create_collection_returns_instance_of_promise(self):
+        self.assertIsInstance(self.collection.create_collection(), Promise)
 
     def test_create_collection_returns_collection_id(self):
-        self.assertIsNotNone(self.collection.create_collection())
+        self.collection.create_collection().done(self.c)
+        self.c.assert_called_once_with(mock.ANY)
 
     def test_create_collection_does_nothing_when_collection_Exists(self):
-        idx = self.collection.create_collection()
-        idxb = self.collection.store_item(idx, 'b')
-        self.assertEqual(self.collection.create_collection(idx), idx)
-        c = self.collection.read_collection(idx)
-        self.assertTrue(idxb in c)
-        self.assertEqual(c[idxb], 'b')
+        idx = self._idx(self.collection.create_collection())
+        idxb = self._idx(self.collection.store_item(idx, 'b'))
+        idx2 = self._idx(self.collection.create_collection(idx))
+        self.assertEqual(idx, idx2)
+        self.collection.read_collection(idx).done(self.c)
+        self.c.assert_called_once_with({idxb: 'b'})
 
     def test_create_collection_generates_unique_id_for_item(self):
-        idx1 = self.collection.create_collection(0)
-        idx2 = self.collection.create_collection()
+        idx1 = self._idx(self.collection.create_collection(0).done(self.c))
+        idx2 = self._idx(self.collection.create_collection().done(self.c))
         self.assertNotEqual(idx1, idx2)
 
+    def test_read_collection_returns_instance_of_promise(self):
+        self.assertIsInstance(self.collection.read_collection(1), Promise)
+
+    def test_read_collection_raises_key_error_for_non_existing_collection(self):
+        self.collection.read_collection('foo').fail(self.c).done(self.c)
+        self.c.assert_called_once_with(exception=IsA(KeyError))
+
+
     def test_read_collection_returns_dict_for_existing_collection(self):
-        idx = self.collection.create_collection()
-        self.assertEqual(self.collection.read_collection(idx), {})
+        idx = self._idx(self.collection.create_collection())
+        self.collection.read_collection(idx).done(self.c)
+        self.c.assert_called_once_with({})
 
     def test_read_collection_returns_item_added_to_collection(self):
-        idx = self.collection.create_collection()
-        idxa = self.collection.store_item(idx, 'a')
-        idxb = self.collection.store_item(idx, 'b')
-        c = self.collection.read_collection(idx)
-        self.assertTrue(idxa in c)
-        self.assertTrue(idxb in c)
-        self.assertEqual(c[idxa], 'a')
-        self.assertEqual(c[idxb], 'b')
+        idx = self._idx(self.collection.create_collection())
+        idxa = self._idx(self.collection.store_item(idx, 'a'))
+        idxb = self._idx(self.collection.store_item(idx, 'b'))
+        self.collection.read_collection(idx).done(self.c)
+        self.c.assert_called_once_with({idxa: 'a', idxb: 'b'})
 
     def test_store_item_expects_collection_to_exist(self):
-        self.assertRaises(KeyError, partial(self.collection.store_item, 'foo',
-                None))
+        self.collection.store_item('foo', None).fail(self.c)
+        self.c.assert_called_once_with(exception=IsA(KeyError))
 
     def test_store_item_returns_item_id(self):
-        idx = self.collection.create_collection()
-        self.assertIsNotNone(self.collection.store_item(idx, None))
+        idx = self._idx(self.collection.create_collection())
+        idxc = self.collection.store_item(idx, None)
+        self.assertIsNotNone(idxc)
 
     def test_remove_item_expects_collection_to_exist(self):
-        self.assertRaises(KeyError, partial(self.collection.remove_item,
-                'collection', 'item'))
+        self.collection.remove_item('collection', 'item').fail(self.c)
+        self.c.assert_called_once_with(exception=IsA(KeyError))
+
+    def test_remove_item_returns_id_of_removed_collection_and_item(self):
+        idx = self._idx(self.collection.create_collection())
+        idxi = self._idx(self.collection.store_item(idx, None))
+        self.collection.remove_item(idx, idxi).done(self.c)
+        self.c.assert_called_once_with((idx, idxi))
 
     def test_remove_item_removes_item(self):
         colid = 'col'
@@ -75,12 +98,17 @@ class MemoryTestCase(unittest.TestCase):
         self.collection.create_collection(colid)
         self.collection.store_item(colid, 'foo', itemid)
         self.collection.remove_item(colid, itemid)
-        self.assertFalse(itemid in self.collection.read_collection(colid))
-
+        self.collection.read_collection(colid).done(self.c)
+        self.c.assert_called_once_with({})
 
     def test_remove_collection_expects_collection_to_exist(self):
-        self.assertRaises(KeyError, partial(self.collection.remove_collection,
-                'collection'))
+        self.collection.remove_collection('collection').fail(self.c)
+        self.c.assert_called_once_with(exception=IsA(KeyError))
+
+    def test_remove_collection_returns_id_of_removed_collection(self):
+        idx = self._idx(self.collection.create_collection())
+        self.collection.remove_collection(idx).done(self.c)
+        self.c.assert_called_once_with(idx)
 
     def test_remove_collection_removes_all_items(self):
         colid = 'col'
@@ -88,8 +116,8 @@ class MemoryTestCase(unittest.TestCase):
         self.collection.create_collection(colid)
         self.collection.store_item(colid, 'foo', itemid)
         self.collection.remove_collection(colid)
-        self.assertRaises(KeyError, partial(self.collection.read_collection,
-                colid))
+        self.collection.read_collection(colid).fail(self.c)
+        self.c.assert_called_once_with(exception=IsA(KeyError))
 
     def test_store_item_generates_unique_item_id(self):
         self.collection.create_collection('a')
@@ -100,8 +128,8 @@ class MemoryTestCase(unittest.TestCase):
     def test_store_item_generates_item_id_unique_to_given_collection(self):
         self.collection.create_collection('a')
         self.collection.create_collection('b')
-        idx1 = self.collection.store_item('a', None, 0)
-        idx2 = self.collection.store_item('b', None)
+        idx1 = self._idx(self.collection.store_item('a', None, 0))
+        idx2 = self._idx(self.collection.store_item('b', None))
         self.assertEqual(idx1, idx2)
 
 
