0312c7fc9370d738adaa550e13ccf6b57488ee1c
Michał Bachowski <michal@bachowski.pl>
2013-06-10 09:41:17 +0200

    Refactored contentbits.manager to work in fully asynchornous manner

--- src/contentbits/manager.py
+++ src/contentbits/manager.py
@@ -1,9 +1,71 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from functools import partial
+from promise import Deferred
 from contentbits.factory import CollectionFactory
 
 
 class Manager(object):
+
+    class CollectionSaveHelper(object):
+
+        def __init__(self, manager, new_collection):
+            self.deferred = Deferred()
+            self._manager = manager
+            self.new_collection = new_collection
+            self.old_collection = None
+            self.items = iter(new_collection)
+
+        def __next__(self):
+            return next(self.items)
+
+        def save(self):
+            # read current state of collection (it`s items)
+            # if collection exists - go to next step
+            # otherwise create new collection
+            self._manager.storage.read_collection(self.new_collection.id)\
+                    .done(partial(self._collection_found))\
+                    .fail(partial(self._collection_not_found))
+
+            return self.deferred.promise()
+
+        def _collection_found(self, old_collection):
+            self.old_collection = self._manager.collection_factory.discover(old_collection)
+            self._add_items()
+
+        def _collection_not_found(self, exception):
+            self._manager.storage.create_collection(self.new_collection.id)\
+                    .done(partial(self._collection_created))\
+                    .fail(self.deferred.reject)
+
+        def _collection_created(self, collection_id):
+            self.new_collection.id = collection_id
+            # lie that old collection was just empty
+            self._collection_found([])
+
+        def _add_items(self):
+            try:
+                self._manager.add_item_to_collection(self.new_collection.id,
+                        next(self)).done(self._item_added)\
+                        .fail(self.deferred.reject)
+            except StopIteration:
+                self._del_items()
+
+        def _item_added(self, item_id):
+            try:
+                del self.old_collection[item_id]
+            except:
+                pass
+            self._add_items()
+
+        def _del_items(self, deleted_item_id=None):
+            try:
+                self._manager.remove_item_from_collection(
+                        self.new_collection.id, next(self.old_collection).id)\
+                        .done(partial(self._del_items))\
+                        .fail(self.deferred.reject)
+            except StopIteration:
+                self.deferred.resolve(self.new_collection.id)
 
     def __init__(self, storage=None, collection_factory=None):
         self._storage = None
@@ -22,41 +84,25 @@ class Manager(object):
         return self
 
     def save_collection(self, collection):
-        # read collection
-        try:
-            old_collection = self._storage.read_collection(collection.id)
-        except KeyError:
-            old_collection = {}
-            collection.id = self._storage.create_collection(collection.id)
-        old_collection = self._collection_factory.discover(old_collection)
-        # add new items
-        for item in collection:
-            item.id = self.add_item_to_collection(collection.id, item)
-            try:
-                del old_collection[item]
-            except ValueError:
-                pass
-        # remove old items
-        for item in old_collection:
-            self.remove_item_from_collection(collection.id, item.id)
-        return collection.id
+        return Manager.CollectionSaveHelper(self, collection).save()
 
     def read_collection(self, collection_id):
-        try:
-            collection = self._storage.read_collection(collection_id)
-        except KeyError:
-            collection = {}
-        c = self._collection_factory.discover(collection)
-        c.id = collection_id
-        return c
+        deferred = Deferred()
+
+        def _done(collection):
+            c = self._collection_factory.discover(collection)
+            c.id = collection_id
+            deferred.resolve(c)
+
+        self._storage.read_collection(collection_id)\
+                .done(_done).fail(deferred.reject)
+        return deferred.promise()
 
     def remove_collection(self, collection_id):
-        self._storage.remove_collection(collection_id)
-        return self
+        return self._storage.remove_collection(collection_id)
 
     def remove_item_from_collection(self, collection_id, item_id):
-        self._storage.remove_item(collection_id, item_id)
-        return self
+        return self._storage.remove_item(collection_id, item_id)
 
     def add_item_to_collection(self, collection_id, item):
         return self._storage.store_item(collection_id, item.data, item.id)
--- test/manager_test.py
+++ test/manager_test.py
@@ -7,23 +7,46 @@
 import unittest
 from functools import partial
 
-# hack for loading modules
-from _path import fix, mock
-fix()
+##
+# test utilities
+#
+from testutils import mock, IsCallable
 
 ##
 # content bits modules
 #
 from contentbits.manager import Manager
+from contentbits.factory import CollectionFactory
 
 
 class ManagerTestCase(unittest.TestCase):
 
     def setUp(self):
+        # set defaul exception name for "fail_side_effect"
+        self.exception = KeyError()
+
+        # setup promise
+        self.promise = mock.MagicMock()
+        self.promise.done = mock.MagicMock(side_effect=lambda a:
+                [a({'a': 1}), self.promise][1])
+
+        # setup storage
         self.storage = mock.MagicMock()
-        self.factory = mock.MagicMock()
+        self.storage.read_collection = mock.MagicMock(return_value=self.promise)
+
+        promise = mock.MagicMock()
+        promise.done = lambda a: [a(self.collection.id), self.promise][1]
+        self.storage.create_collection = mock.MagicMock(return_value=promise)
+
+        # setup collection
         self.collection = mock.MagicMock()
         self.collection.id = 1
+
+        # setup factory
+        self.factory = mock.MagicMock()
+        self.factory.discover = mock.MagicMock(return_value=self.collection)
+
+        # setup manager
         self.manager = Manager(self.storage, self.factory)
 
     def test_init_allows_to_pass_2_arguments(self):
@@ -45,36 +68,41 @@ class ManagerTestCase(unittest.TestCase):
         Manager().set_storage(storage).add_item_to_collection(None, item)
         storage.store_item.assert_called_once_with(None, None, None)
 
+    def test_storage_defaults_to_None(self):
+        self.assertIsNone(Manager().storage)
+
+    def test_storage_returns_previously_set_storage(self):
+        self.assertEqual(Manager().set_storage('foo').storage, 'foo')
+
     def test_set_collection_factory_sets_collection_factory_to_be_used(self):
-        collection = mock.MagicMock()
-        cf = mock.MagicMock()
-        cf.discover = mock.MagicMock(return_value=collection)
-        self.storage.read_collection = mock.MagicMock(return_value={'a': 1})
-        c = Manager(self.storage).set_collection_factory(cf).read_collection(
-                None)
-        self.assertIsNone(c.id)
-        cf.discover.assert_called_once_with({'a': 1})
+        Manager(self.storage).set_collection_factory(self.factory).read_collection(None)
+        self.factory.discover.assert_called_once_with({'a': 1})
+
+    def test_collection_factory_defaults_to_CollectionFactory(self):
+        self.assertIsInstance(Manager().collection_factory, CollectionFactory)
+
+    def test_collection_factory_returns_previously_set_collection_factory(self):
+        self.assertEqual(Manager().set_collection_factory('foo').collection_factory, 'foo')
 
     def test_read_collection_uses_factory_and_storage(self):
-        self.storage.read_collection = mock.MagicMock(return_value={'a': 1})
-        self.factory.discover = mock.MagicMock(return_value=self.collection)
-        c = self.manager.read_collection(1)
+        self.manager.read_collection(1)
         self.storage.read_collection.assert_called_once_with(1)
         self.factory.discover.assert_called_once_with({'a': 1})
-        self.assertEqual(c, self.collection)
 
     def test_read_collection_appends_id(self):
-        self.storage.read_collection = mock.MagicMock(return_value={'a': 1})
-        self.factory.discover = mock.MagicMock(return_value=self.collection)
-        c = self.manager.read_collection(1)
-        self.assertEqual(c.id, 1)
+        self.manager.read_collection(1)
+        self.assertEqual(self.collection.id, 1)
 
     def test_read_collection_returns_collection_when_no_collection_found(self):
+        # turn off default behavious for "done" callback
+        self.promise.done = lambda a: self.promise
+        self.promise.fail = mock.MagicMock(side_effect=lambda a:
+                [a(exception=self.exception), self.promise][1])
+        self.manager.read_collection(1)
+        self.assertEqual(self.collection.id, 1)
 
-        self.storage.read_collection = mock.MagicMock(side_effect=KeyError())
-        self.factory.discover = mock.MagicMock(return_value=self.collection)
-        c = self.manager.read_collection(1)
-        self.assertEqual(c.id, 1)
+    def test_remove_collection_expects_one_argument(self):
+        self.assertRaises(TypeError, self.manager.remove_collection)
 
     def test_remove_collection_uses_storage(self):
         self.storage.remove_collection = mock.MagicMock()
@@ -105,45 +133,38 @@ class ManagerTestCase(unittest.TestCase):
         self.assertRaises(TypeError, self.manager.save_collection)
 
     def test_save_collection_requires_1_argument_1(self):
-        self.assertRaises(AttributeError,
+        self.assertRaises(TypeError,
+                partial(self.manager.save_collection, None))
+
+    def test_save_collection_expects_iterable_object(self):
+        self.assertRaises(TypeError,
                 partial(self.manager.save_collection, None))
 
     def test_save_collection_expects_object_with_id_attribute(self):
-        self.assertRaises(AttributeError,
-                partial(self.manager.save_collection, None))
-        err = False
-        try:
-            self.manager.save_collection(self.collection)
-        except:
-            err = True
-        self.assertFalse(err)
-
-    def test_save_collection_expects_iterable_object(self):
-        self.collection.__iter__ = mock.MagicMock(return_value=iter([]))
-        self.manager.save_collection(self.collection)
-        self.collection.__iter__.assert_called_once_with()
+        del self.collection.id
+        self.assertRaises(AttributeError,partial(self.manager.save_collection,
+                self.collection))
 
     def test_save_collection_reads_collection_from_storage(self):
-        self.storage.read_collection = mock.MagicMock(return_value=iter([]))
         self.manager.save_collection(self.collection)
         self.storage.read_collection.assert_called_once_with(self.collection.id)
 
     def test_save_collection_creates_collection_when_not_found(self):
-        self.storage.read_collection = mock.MagicMock(side_effect=KeyError())
-        self.storage.create_collection = mock.MagicMock(
-                return_value=self.collection.id)
+        self.promise.done = lambda a: self.promise
+        self.promise.fail = mock.MagicMock(side_effect=lambda a:
+                [a(exception=self.exception), self.promise][1])
         self.manager.save_collection(self.collection)
         self.storage.read_collection.assert_called_once_with(self.collection.id)
-        self.storage.create_collection.assert_called_once_with(
-                self.collection.id)
+        self.storage.create_collection.assert_called_once_with(self.collection.id)
 
     def test_save_collection_stores_new_items(self):
         # prepare
         item = mock.MagicMock()
         item.id = 2
         item.data = 'foo'
-        self.collection.__iter__ = mock.MagicMock(return_value=iter([item]))
-        self.storage.read_collection = mock.MagicMock(return_value={})
+        self.collection.__iter__ = lambda a: iter([item])
+        self.promise.done = lambda a: [a({}), self.promise][1]
+        self.storage.read_collection = lambda a: self.promise
         self.storage.store_item = mock.MagicMock()
         # test
         self.manager.save_collection(self.collection)
@@ -155,27 +176,20 @@ class ManagerTestCase(unittest.TestCase):
         item = mock.MagicMock()
         item.id = 2
         item.data = 'foo'
-        c = mock.MagicMock()
-        c.__iter__ = mock.MagicMock(return_value=iter([item]))
-        self.factory.discover = mock.MagicMock(return_value=c)
-        self.storage.remove_item = mock.MagicMock()
-        # test
-        self.manager.save_collection(self.collection)
-        # verify
-        self.storage.remove_item.assert_called_once_with(1, 2)
+        items = iter([item])
 
-    def test_save_collection_removes_only_old_items_from_collection(self):
-        # prepare
-        item = mock.MagicMock()
-        item.id = 2
-        c = mock.MagicMock()
-        c.__delitem__ = mock.MagicMock(side_effect=ValueError())
-        self.collection.__iter__ = mock.MagicMock(return_value=iter([item]))
-        self.factory.discover = mock.MagicMock(return_value=c)
+        self.storage.read_collection = mock.MagicMock(return_value=self.promise)
+        self.collection.__next__ = mock.MagicMock(side_effect=lambda: next(items))
+
         # test
         self.manager.save_collection(self.collection)
+
         # verify
-        c.__delitem__.assert_called_once_with(item)
+        self.storage.read_collection.assert_called_once_with(1)
+        self.promise.done.assert_called_once_with(IsCallable())
+        self.factory.discover.assert_called_once_with({'a': 1})
+        self.storage.store_item.assert_not_called()
+        self.storage.remove_item.assert_called_once_with(self.collection.id, item.id)
 
 
 if "__main__" == __name__:
